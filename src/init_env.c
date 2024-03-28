1998f3945380b0fe4c3bf8f10d80f54b511b7287
nattensleplubo <nathan.gobert2002@gmail.com>
2024-03-28 22:51:35 +0100

    Env

--- src/init_env.c
+++ src/init_env.c
@@ -1,6 +1,30 @@
 #include "../includes/minishell.h"
 
-int init_env()
+int init_env(char **env)
 {
-	
+  int i;
+  int j;
+  char *curr;
+  t_list new;
+
+  i = 0;
+  while (env[i])
+  {
+    j = 0;
+    while (env[i][j] != '=')
+      j++;
+    curr = malloc(sizeof(char) * j + 1);
+    if (!curr)
+      exit(0);
+    j = 0;
+    while (env[i][j] != '=')
+    {
+      curr[j] = env[i][j];
+      j++;
+    }
+    curr[j] = '\0';
+    new = malloc(sizeof(t_list));
+    if (!new)
+      exit(0);
+  }
 }

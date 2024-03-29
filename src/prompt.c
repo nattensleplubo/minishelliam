0fff1790148420c0cb963d6fe152043e97ea55e7
nattensleplubo <nathan.gobert2002@gmail.com>
2024-03-29 13:22:13 +0100

    cool prompt

--- src/prompt.c
+++ src/prompt.c
@@ -14,8 +14,14 @@
 
 void read_line(void)
 {
+  char *path;
+  char *line;
+
+  path = NULL;
+  path = getcwd(NULL, 100);
+  line = ft_strjoin(path, " $ ");
   free(_ms(0)->prompt);
-  _ms(0)->prompt = readline("shell $ ");
+  _ms(0)->prompt = readline(line);
   if (!_ms(0)->prompt)
   {
     rl_clear_history();
@@ -24,4 +30,6 @@ void read_line(void)
   }
   if (strlen(_ms(0)->prompt) != 0) //! REPLACE BY LIBFT FUNCTION
     add_history(_ms(0)->prompt);
+  free(path);
+  free(line);
 }

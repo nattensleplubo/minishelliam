0f0ab11d1f8e5422a03000548c09d6c53ba59298
fylez <superliamzaengel@gmail.com>
2024-03-29 13:50:51 +0100

    added cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: lzaengel <lzaengel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 12:11:27 by lzaengel          #+#    #+#             */
-/*   Updated: 2024/03/29 12:16:02 by lzaengel         ###   ########.fr       */
+/*   Updated: 2024/03/29 13:49:45 by lzaengel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 void	ft_cd(char	*dirtogo)
 {
-	chdir(dirtogo);
+	if(dirtogo == NULL)
+		chdir("/home/");
+	else
+		chdir(dirtogo);
 }

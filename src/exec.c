b53fc5011df03ee4bb3de26f302cce7f2a6606dd
nattensleplubo <nathan.gobert2002@gmail.com>
2024-05-28 14:22:31 +0000

    caca

--- src/exec.c
+++ src/exec.c
@@ -30,7 +30,26 @@ int get_number_of_commands(void) {
   return i;
 }
 
-int get_number_of_args(int i) { int ret_size; }
+int get_number_of_args(int i) {
+  int ret_size;
+  int j;
+  t_list *temp;
+
+  j = 0;
+  ret_size = 1;
+  temp = _ms(0)->tokenized_prompt;
+  while (j != i) {
+    if (ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3))
+      j++;
+    temp = temp->next;
+  }
+  while (!ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) && temp) {
+    if (ft_strncmp(((t_quote *)temp->content)->token, "arg", 3))
+      ret_size++;
+    temp = temp->next;
+  }
+  return ret_size;
+}
 
 char ***make_commands_tab() {
   char ***ret;
@@ -39,11 +58,21 @@ char ***make_commands_tab() {
 
   temp = _ms(0)->tokenized_prompt;
   ints[1] = 0;
-  ints[0] = get_number_of_commands();
-  ret = malloc(sizeof(char **) * (ints[0] + 1));
-  while (ints[1] < ints[0]) {
-    if ()
-      ints[2] = get_number_of_args(ints[1]);
+  ints[0] = get_number_of_commands(); // Calcul le nombre de commandes qu'il y
+                                      // aura a executer
+  ret = malloc(sizeof(char **) *
+               (ints[0] + 1)); // Allocation de tableau de tableau de strings
+  while (ints[1] <
+         ints[0]) { // Tant que l'index est inferieur au nombre de commandes
+    if (ft_strncmp(((t_quote *)temp->content)->token, "cmd",
+                   3)) { // Si on tombe sur un commande
+      ints[2] = get_number_of_args(
+          ints[1]); // Calcul le nombre d'arguments d'une commande
+      ret[ints[1]] = malloc(sizeof(char *) *
+                            (ints[2] + 1)); // Allocation de tableau de strings
+      ints[1]++;
+    }
+    temp = temp->next;
   }
   return ret;
 }

5462967f3ee06ff0de1c7b99c9a3f17f7b5ef4ac
fylez <superliamzaengel@gmail.com>
2024-05-29 20:24:15 +0200

    fixed export and added unset

--- builtins/export.c
+++ builtins/export.c
@@ -1,55 +1,54 @@
 #include "../includes/minishell.h"
 
 
-void	changeValue(char **OldString, char *NewString)
+void	changeValue(char **oldstring, char *newstring)
 {
 	int	size;
 	int	i;
 
 	i = 0;
-	size = ft_strlen(NewString);
-	free(*OldString);
-	*OldString = malloc(sizeof(char) * size + 1);
-	while (NewString[i])
+	size = ft_strlen(newstring);
+	free(*oldstring);
+	*oldstring = malloc(sizeof(char) * size + 1);
+	while (newstring[i])
 	{
-		(*OldString)[i] = NewString[i];
+		(*oldstring)[i] = newstring[i];
 		i++;
 	}
-	(*OldString)[i] = '\0';
+	(*oldstring)[i] = '\0';
 }
 
-void	addToTab(char ***tab, const char *newString)
+void	addToTab(char ***tab, const char *newstring)
 {
 	int		size;
 	int		i;
-	char	**newArray;
+	char	**newarray;
 
 	i = 0;
 	size = 0;
 	while ((*tab)[size] != NULL)
 		size++;
-	newArray = malloc((size + 2) * sizeof(char *));
-	if (newArray == NULL)
+	newarray = malloc((size + 2) * sizeof(char *));
+	if (newarray == NULL)
 	{}
 	while ((*tab)[i])
 	{
-		newArray[i] = (*tab)[i];
+		newarray[i] = (*tab)[i];
 		i++;
 	}
-    newArray[size] = malloc((strlen(newString) + 1) * sizeof(char));
-    if (newArray[size] == NULL)
+	newarray[size] = malloc((strlen(newstring) + 1) * sizeof(char));
+	if (newarray[size] == NULL)
 	{}
-    strcpy(newArray[size], newString);
-    newArray[size + 1] = NULL;
-    free(*tab);
-    *tab = newArray;
+	strcpy(newarray[size], newstring);
+	newarray[size + 1] = NULL;
+	free(*tab);
+	*tab = newarray;
 }
 
 void	ft_export(char **arg)
 {
-	int		i[2];
+	int		i[3];
 	char	*env;
-	int		indexenv;
 
 	i[0] = 1;
 	while (arg[i[0]])
@@ -61,14 +60,13 @@ void	ft_export(char **arg)
 		if (!env)
 		{}
 		ft_strlcpy(env, arg[i[0]], i[1] + 1);
-		indexenv = get_index_of_varname(env);
-		if (arg[i[0]][i[1]] == '=')
-		{
-			if (indexenv == -1)
-				addToTab(&_ms(0)->env, arg[i[0]]);
-			else if (indexenv >= 0)
-				changeValue(&_ms(0)->env[indexenv], arg[i[0]]);
-		}
+		i[2] = get_index_of_varname(env);
+		if (arg[i[0]][0] == '=')
+		{} //error
+		else if (i[2] == -1 && arg[i[0]][i[1]] == '=')
+			addToTab(&_ms(0)->env, arg[i[0]]);
+		else if (i[2] >= 0 && arg[i[0]][i[1]] == '=')
+			changeValue(&_ms(0)->env[i[2]], arg[i[0]]);
 		i[0]++;
 		free(env);
 	}
---
+++ builtins/unset.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lzaengel <lzaengel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 19:02:40 by lzaengel          #+#    #+#             */
+/*   Updated: 2024/05/29 20:22:19 by lzaengel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	deletefromtab(char ***tab, int index)
+{
+	int		size;
+	int		i;
+	int		j;
+	char	**newarray;
+
+	i = 0;
+	j = 0;
+	size = 0;
+	while ((*tab)[size] != NULL)
+		size++;
+	newarray = malloc((size) * sizeof(char *));
+	if (newarray == NULL)
+	{}
+	while ((*tab)[j])
+	{
+		if (j != index)
+			newarray[i++] = (*tab)[j];
+		j++;
+	}
+	newarray[size - 1] = NULL;
+	free((*tab)[index]);
+	free(*tab);
+	*tab = newarray;
+}
+
+void	ft_unset(char **arg)
+{
+	int	i;
+	int	index;
+
+	i = 1;
+	while(arg[i])
+	{
+		index = get_index_of_varname(arg[i]);
+		if(index >= 0)
+			deletefromtab(&_ms(0)->env, index);
+		i++;
+	}
+}

3d4ba45e6d7f2ad0c4f447265333483f33f94d6c
fylez <superliamzaengel@gmail.com>
2024-06-03 19:38:16 +0200

    norme_c_h_i_n_e

--- main.c
+++ main.c
@@ -6,77 +6,83 @@
 /*   By: lzaengel <lzaengel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 12:20:26 by lzaengel          #+#    #+#             */
-/*   Updated: 2024/05/22 17:15:50 by lzaengel         ###   ########.fr       */
+/*   Updated: 2024/06/03 19:36:57 by lzaengel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/minishell.h"
 
-t_minishell *
-_ms(int params) // Global function 1 to print it's values. 0 To do nothing
+t_minishell	*_ms(int params) // Global function 1 to print it's values. 0 To do nothing
 {
-  static t_minishell *ms = NULL;
-  (void)params;
+	static t_minishell	*ms = NULL;
 
-  if (!ms) {
-    ms = malloc(sizeof(t_minishell));
-    if (!ms)
-      exit(0);
-  }
-  return (ms);
+	(void)params;
+	if (!ms)
+	{
+		ms = malloc(sizeof(t_minishell));
+		if (!ms)
+			exit(0);
+	}
+	return (ms);
 }
 
-void ft_lstprint(t_list *lst) {
-  printf("TOKENIZED PROMPT :\n");
-  t_list *temp;
-
-  if (lst == NULL)
-    return;
-  temp = lst;
-  printf("TOKEN[%s]\t|\tCONTENT[%s]\n", ((t_quote *)temp->content)->token,
-         ((t_quote *)temp->content)->str);
-  while (temp->next) {
-    temp = temp->next;
-    printf("TOKEN[%s]\t|\tCONTENT[%s]\n", ((t_quote *)temp->content)->token,
-           ((t_quote *)temp->content)->str);
-  }
-  printf("----------------------------------------------\n");
-}
-
-void init_ms(void) // Initialize all values to some default ones at the
-                   // beginning of the program
+void	ft_lstprint(t_list *lst)
 {
-  _ms(0)->prompt = NULL;
-  _ms(0)->splitted_prompt = NULL;
-  _ms(0)->env = NULL;
-  _ms(0)->tokenized_prompt = NULL;
+	t_list	*temp;
+
+	printf("TOKENIZED PROMPT :\n");
+	if (lst == NULL)
+		return ;
+	temp = lst;
+	printf("TOKEN[%s]\t|\tCONTENT[%s]\n", ((t_quote *)temp->content)->token,
+		((t_quote *)temp->content)->str);
+	while (temp->next)
+	{
+		temp = temp->next;
+		printf("TOKEN[%s]\t|\tCONTENT[%s]\n", ((t_quote *)temp->content)->token,
+			((t_quote *)temp->content)->str);
+	}
+	printf("----------------------------------------------\n");
 }
-void ft_bash(void) {
-  if (read_line()) {
-    expend_env_vars();
-    _ms(0)->splitted_prompt = prompt_splitter(_ms(0)->prompt);
-    // print_tab(_ms(0)->splitted_prompt);
-    ft_token();
-    // ft_lstprint(_ms(0)->tokenized_prompt);
-    // printf("%d\n", ft_lstsize(_ms(0)->tokenized_prompt));
-    delete_quotes();
-    print_tab(_ms(0)->splitted_prompt);
-    exec();
-  }
+
+void	init_ms(void) // Initialize all values to some default ones at the
+{
+	_ms(0)->prompt = NULL; // beginning of the program
+	_ms(0)->splitted_prompt = NULL;
+	_ms(0)->env = NULL;
+	_ms(0)->tokenized_prompt = NULL;
 }
-int main(int argc, char **argv, char **envp) {
-  (void)argc;
-  (void)argv;
-  struct sigaction s;
-  (void)argc;
-  (void)argv;
-  s.sa_handler = ft_signal;
-  rl_catch_signals = 0;
-  sigaction(SIGINT, &s, NULL);
-  sigaction(SIGQUIT, &s, NULL);
-  init_ms();
-  init_env(envp);
-  while (9) {
-    ft_bash();
-  }
+
+void	ft_bash(void)
+{
+	if (read_line())
+	{
+		expend_env_vars();
+		_ms(0)->splitted_prompt = prompt_splitter(_ms(0)->prompt);
+		// print_tab(_ms(0)->splitted_prompt);
+		ft_token();
+		// ft_lstprint(_ms(0)->tokenized_prompt);
+		// printf("%d\n", ft_lstsize(_ms(0)->tokenized_prompt));
+		delete_quotes();
+		print_tab(_ms(0)->splitted_prompt);
+		exec();
+	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	struct sigaction	s;
+
+	(void)argc;
+	(void)argv;
+	(void)argc;
+	(void)argv;
+	s.sa_handler = ft_signal;
+	rl_catch_signals = 0;
+	sigaction(SIGINT, &s, NULL);
+	sigaction(SIGQUIT, &s, NULL);
+	init_ms();
+	init_env(envp);
+	while (9)
+		ft_bash();
 }
--- src/exec.c
+++ src/exec.c
@@ -1,114 +1,127 @@
 #include "../includes/minishell.h"
 
-void fill_list(void) {
-  int i;
-  int size;
-  t_list *temp;
+void	fill_list(void)
+{
+	int		i;
+	int		size;
+	t_list	*temp;
 
-  temp = _ms(0)->tokenized_prompt;
-  size = tab_size(_ms(0)->splitted_prompt);
-  i = 0;
-  while (i < size) {
-    ((t_quote *)temp->content)->str =
-        ft_calloc(sizeof(char), ft_strlen(_ms(0)->splitted_prompt[i]));
-    ft_strlcpy(((t_quote *)temp->content)->str, _ms(0)->splitted_prompt[i],
-               ft_strlen(_ms(0)->splitted_prompt[i]) + 1);
-    i++;
-    temp = temp->next;
-  }
+	temp = _ms(0)->tokenized_prompt;
+	size = tab_size(_ms(0)->splitted_prompt);
+	i = 0;
+	while (i < size)
+	{
+		((t_quote *)temp->content)->str
+			= ft_calloc(sizeof(char), ft_strlen(_ms(0)->splitted_prompt[i]));
+		ft_strlcpy(((t_quote *)temp->content)->str, _ms(0)->splitted_prompt[i],
+			ft_strlen(_ms(0)->splitted_prompt[i]) + 1);
+	i++;
+	temp = temp->next;
+	}
 }
 
-int get_number_of_commands(void) {
-  t_list *temp;
-  int i;
+int	get_number_of_commands(void)
+{
+	t_list	*temp;
+	int		i;
 
-  i = 0;
-
-  temp = _ms(0)->tokenized_prompt;
-  while (temp) {
-    if (ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) == 0) {
-      i++;
-    }
-    temp = temp->next;
-  }
-  return i;
+	i = 0;
+	temp = _ms(0)->tokenized_prompt;
+	while (temp)
+	{
+		if (ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) == 0)
+			i++;
+		temp = temp->next;
+	}
+	return (i);
 }
 
-int get_number_of_args(int i) {
-  int ret_size;
-  int j;
-  t_list *temp;
+int	get_number_of_args(int i)
+{
+	int		ret_size;
+	int		j;
+	t_list	*temp;
 
-  j = 0;
-  ret_size = 0;
-  temp = _ms(0)->tokenized_prompt;
-  while (j <= i) {
-    if (ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) == 0) {
-      j++;
-    }
-    temp = temp->next;
-  }
-  while (temp && ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) != 0) {
-    if (ft_strncmp(((t_quote *)temp->content)->token, "arg", 3) == 0)
-      ret_size++;
-    temp = temp->next;
-  }
-  return ret_size;
+	j = 0;
+	ret_size = 0;
+	temp = _ms(0)->tokenized_prompt;
+	while (j <= i)
+	{
+		if (ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) == 0)
+			j++;
+		temp = temp->next;
+	}
+	while (temp && ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) != 0)
+	{
+		if (ft_strncmp(((t_quote *)temp->content)->token, "arg", 3) == 0)
+			ret_size++;
+		temp = temp->next;
+	}
+	return (ret_size);
 }
 
-char ***make_commands_tab() {
-  char ***ret;
-  int ints[5]; // 0: cmd, 1: i, 2: tab_size, 3 fill_commands, 4 check commands
-  t_list *temp;
+char	***make_commands_tab(void)
+{
+	char	***ret;
+	t_list	*temp;
+	int		ints[5]; // 0: cmd, 1: i, 2: tab_size, 3 fill_commands, 4 check commands
 
-  temp = _ms(0)->tokenized_prompt;
-  ints[1] = 0;
-  ints[4] = -1;
-  ints[0] = get_number_of_commands();
-  printf("There is %d commands\n", ints[0]);
-  ret = (char ***)ft_calloc(sizeof(char **), (ints[0] + 1));
-  ret[ints[0]] = NULL;
-  while (temp && ints[4] < ints[0]) {
-    if (ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) == 0) {
-      ints[4]++;
-      ints[3] = 1;
-      ints[2] = get_number_of_args(ints[1]);
-      printf("There is %d args\n", ints[2]);
-      ret[ints[1]] = malloc(sizeof(char *) * (ints[2] + 2));
-      ret[ints[1]][ints[2] + 1] = NULL;
-      ret[ints[1]][0] =
-          ft_calloc(sizeof(char), ft_strlen(((t_quote *)temp->content)->str));
-      ft_strlcpy(ret[ints[1]][0], ((t_quote *)temp->content)->str,
-                 ft_strlen(((t_quote *)temp->content)->str) + 1);
-      ints[1]++;
-    }
-    if (ft_strncmp(((t_quote *)temp->content)->token, "arg", 3) == 0) {
-      ret[ints[4]][ints[3]] =
-          ft_calloc(sizeof(char), ft_strlen(((t_quote *)temp->content)->str));
-      ft_strlcpy(ret[ints[4]][ints[3]], ((t_quote *)temp->content)->str,
-                 ft_strlen(((t_quote *)temp->content)->str) + 1);
-      ints[3]++;
-    }
-    temp = temp->next;
-  }
-  return ret;
+
+	temp = _ms(0)->tokenized_prompt;
+	ints[1] = 0;
+	ints[4] = -1;
+	ints[0] = get_number_of_commands();
+	printf("There is %d commands\n", ints[0]);
+	ret = (char ***)ft_calloc(sizeof(char **), (ints[0] + 1));
+	ret[ints[0]] = NULL;
+	while (temp && ints[4] < ints[0])
+	{
+		if (ft_strncmp(((t_quote *)temp->content)->token, "cmd", 3) == 0)
+		{
+			ints[4]++;
+			ints[3] = 1;
+			ints[2] = get_number_of_args(ints[1]);
+			printf("There is %d args\n", ints[2]);
+			ret[ints[1]] = malloc(sizeof(char *) * (ints[2] + 2));
+			ret[ints[1]][ints[2] + 1] = NULL;
+			ret[ints[1]][0]
+				= ft_calloc(sizeof(char), ft_strlen
+					(((t_quote *)temp->content)->str));
+			ft_strlcpy(ret[ints[1]][0], ((t_quote *)temp->content)->str,
+				ft_strlen(((t_quote *)temp->content)->str) + 1);
+			ints[1]++;
+		}
+		if (ft_strncmp(((t_quote *)temp->content)->token, "arg", 3) == 0)
+		{
+			ret[ints[4]][ints[3]]
+				= ft_calloc(sizeof(char), ft_strlen(((t_quote *)temp->content)->str));
+			ft_strlcpy(ret[ints[4]][ints[3]], ((t_quote *)temp->content)->str,
+				ft_strlen(((t_quote *)temp->content)->str) + 1);
+			ints[3]++;
+		}
+		temp = temp->next;
+	}
+	return (ret);
 }
 
-void exec(void) {
-  int i;
-  int j;
-  fill_list();
-  ft_lstprint(_ms(0)->tokenized_prompt);
-  _ms(0)->commands = make_commands_tab();
-  char ***test = _ms(0)->commands;
-  i = 0;
-  j = 0;
-  while (test[i] != NULL)
-    i++;
+void	exec(void)
+{
+	int		i;
+	int		j;
+	char	***test;
 
-  while (j < i) {
-    print_tab(test[j]);
-    j++;
-    printf("-----\n");
-  }
+	fill_list();
+	ft_lstprint(_ms(0)->tokenized_prompt);
+	_ms(0)->commands = make_commands_tab();
+	test = _ms(0)->commands;
+	i = 0;
+	j = 0;
+	while (test[i] != NULL)
+		i++;
+	while (j < i)
+	{
+		print_tab(test[j]);
+		j++;
+		printf("-----\n");
+	}
 }
